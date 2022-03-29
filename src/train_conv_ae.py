6fca2f4eef469f2e0e07684ca8f81188a3714d77
danqiye1 <yedanqi@example.com>
2022-03-29 16:42:44 +0800

    Add wandb configurations

--- src/train_conv_ae.py
+++ src/train_conv_ae.py
@@ -6,6 +6,8 @@ import argparse
 import numpy as np
 from tqdm import tqdm
 
+import wandb
+
 from datasets.datasets import load_all_datasets
 
 parser = argparse.ArgumentParser()
@@ -15,22 +17,19 @@ parser.add_argument("--dataset_type", type=str, help="Pick which dataset to use:
     default="all")
 parser.add_argument("--model_dir", type=str, help="Path to save models",
     default="trained_models")
+parser.add_argument("--wandb_entity", "-e", type=str, help="Weights and Biases entity",
+    default=None)
 
 args = parser.parse_args()
 
 ### Train vanilla convolutional autoencoder ###
 
 # Set up model hyperparameters
-n_bottleneck = 256
-n_flows = 16
-n_epochs = 50
-batch_size = 64
 train_partition_fraction = 1.0
 identifier_str = 'ae_v1'
 
 enable_cuda = True
 device = torch.device('cuda' if torch.cuda.is_available() and enable_cuda else 'cpu')
-print(device)
 
 # Instantiate a ResNet-based autoencoder
 from pl_bolts.models.autoencoders import AE
@@ -40,6 +39,19 @@ ae = AE(input_height=112, enc_type='resnet18').to(device)
 workdir = os.getcwd()
 full_model_dir = os.path.join(workdir, args.model_dir)
 datasets_map = load_all_datasets(os.path.join(workdir, args.dataset_dir))
+
+# Weights and biases logging
+if args.wandb_entity:
+    wandb.init(project="anomaly_detection", entity=args.wandb_entity)
+    artifact = wandb.Artifact('autoencoder_models', type='models')
+    artifact.add_dir(full_model_dir)
+
+wandb.config = {
+  "learning_rate": 1e-4,
+  "epochs": 20,
+  "batch_size": 64,
+  "weight_decay": 1e-4
+}
 
 if args.dataset_type == "all":
     datasets_list = list(datasets_map.values())
@@ -51,17 +63,17 @@ train_partition_len = int(np.floor(train_partition_fraction * len(train_dataset)
 val_partition_len = len(train_dataset) - train_partition_len
 train_set, val_set = torch.utils.data.random_split(train_dataset, [train_partition_len, val_partition_len])
 
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, drop_last=False)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=wandb.config["batch_size"], shuffle=True, drop_last=False)
 
 print("Loaded data.")
 print("Train set: ", len(train_set))
 print("Val set: ", len(val_set))
 
 # Train the AE
-optimizer =  torch.optim.Adam(ae.parameters(), lr=1e-4, weight_decay=1e-4)
+optimizer =  torch.optim.Adam(ae.parameters(), lr=wandb.config["learning_rate"], weight_decay=wandb.config["weight_decay"])
 loss_fn = nn.MSELoss()
 
-for epoch in range(n_epochs):
+for epoch in range(wandb.config["epochs"]):
     progressbar = tqdm(enumerate(train_loader), total=len(train_loader))
     for batch_n, x in progressbar:
         x = x.to(device)
@@ -76,6 +88,8 @@ for epoch in range(n_epochs):
                 epoch, batch_n * len(x), len(train_loader.dataset),
                        100. * batch_n / len(train_loader),
                        loss.item()))
+
+    wandb.log({"loss": loss.item()})
 
     if epoch % 10 == 0:
         print(f'Saving checkpoint for epoch {epoch}...')
