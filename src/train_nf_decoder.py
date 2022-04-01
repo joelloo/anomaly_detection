d5cab48232f82d2cca075d11a09bb932132af129
Joel Loo <joell@andrew.cmu.edu>
2022-04-01 08:39:21 +0800

    Added training script for ResNet18 decoder.

--- src/train_nf_decoder.py
+++ src/train_nf_decoder.py
@@ -9,8 +9,8 @@ import numpy as np
 from tqdm import tqdm
 
 from datasets.datasets import GazeboSimDataset, load_all_datasets
-from models.realnvp import LatentMaskedAffineCoupling, MLP, NormalisingFlow
 
+from pl_bolts.models.autoencoders.components import resnet18_decoder
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset_dir", type=str, help="Path to dataset",
@@ -23,9 +23,8 @@ parser.add_argument("--model_dir", type=str, help="Path to save models",
 args = parser.parse_args()
 
 ### Train RealNVP normalising flow with frozen ResNet encoder ###
-n_bottleneck = 256
-n_flows = 30
-n_epochs = 30
+n_bottleneck = 1000
+n_epochs = 50
 batch_size = 64
 train_partition_fraction = 1.0
 identifier_str = 'nf'
@@ -35,22 +34,10 @@ device = torch.device('cuda' if torch.cuda.is_available() and enable_cuda else '
 print(f'Device: {device}')
 
 # Initialise and freeze the ResNet18 encoder
-resnet18_encoder = models.resnet18(pretrained=True).to(device)
+encoder = models.resnet18(pretrained=True).to(device)
 
-# Specify the flow layers and construct the normalising flow
-b = torch.tensor(n_bottleneck // 2 * [0, 1] + n_bottleneck % 2 * [0])
-flows = []
-for i in range(n_flows):
-    st_net = MLP(n_bottleneck, 1024, 3)
-    if i % 2 == 0:
-        flows += [LatentMaskedAffineCoupling(b, st_net)]
-    else:
-        flows += [LatentMaskedAffineCoupling(1 - b, st_net)]
-
-prior = torch.distributions.normal.Normal(loc=0.0, scale=1.0)
-
-fc_adapter = nn.Linear(1000, n_bottleneck, bias=True)
-nf = NormalisingFlow(flows, prior, device, first_linear=fc_adapter).to(device)
+# Instantiate the ResNet18 decoder to train
+decoder = resnet18_decoder(n_bottleneck, 112, first_conv=False, maxpool1=False).to(device)
 
 # Load the dataset
 datasets_map = load_all_datasets(args.dataset_dir)
@@ -72,7 +59,7 @@ print("Train set: ", len(train_set))
 print("Val set: ", len(val_set))
 
 # Train the AE
-optimizer =  torch.optim.Adam(nf.parameters(), lr=1e-4, weight_decay=1e-4)
+optimizer =  torch.optim.Adam(decoder.parameters(), lr=1e-4, weight_decay=1e-4)
 loss_fn = nn.MSELoss()
 
 for epoch in range(n_epochs):
@@ -82,10 +69,9 @@ for epoch in range(n_epochs):
         x = x.to(device)
         optimizer.zero_grad()
 
-        z = resnet18_encoder(x)
-        z, log_det = nf(z)
-        log_prob_z = nf.get_prior_log_prob(z)
-        loss = -log_prob_z.mean() - log_det.mean()
+        z = encoder(x)
+        y = decoder(z)
+        loss = loss_fn(y, x)
 
         loss.backward()
         optimizer.step()
@@ -99,7 +85,7 @@ for epoch in range(n_epochs):
 
     if epoch % 10 == 9:
         print(f'Saving checkpoint for epoch {epoch}...')
-        torch.save(nf.state_dict(), os.path.join(args.model_dir, f'{identifier_str}_e{epoch}_size{n_bottleneck}_flows{n_flows}.ckpt'))
+        torch.save(decoder.state_dict(), os.path.join(args.model_dir, f'{identifier_str}_e{epoch}.ckpt'))
 
 # Save the final checkpoint
-torch.save(nf.state_dict(), os.path.join(args.model_dir, f'{identifier_str}_e{epoch}_size{n_bottleneck}_flows{n_flows}.ckpt'))
+torch.save(decoder.state_dict(), os.path.join(args.model_dir, f'{identifier_str}_e{epoch}.ckpt'))
